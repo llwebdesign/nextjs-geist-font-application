34283dbc663dfe4c63b1226430a88ebc3a9cee66
llwebdesign <lennert.loew@gmail.com>
2025-06-05 20:00:44 +0200

    Add server.php 1749146442202

--- server.php
+++ server.php
@@ -3,10 +3,30 @@
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 // Serve static files directly
-if ($uri !== '/' && file_exists(__DIR__ . $uri)) {
-    return false;
+if ($uri !== '/' && file_exists(__DIR__ . '/public' . $uri)) {
+    $extension = pathinfo(__DIR__ . '/public' . $uri, PATHINFO_EXTENSION);
+    
+    // Set correct content type for CSS and JavaScript files
+    switch ($extension) {
+        case 'css':
+            header('Content-Type: text/css');
+            break;
+        case 'js':
+            header('Content-Type: application/javascript');
+            break;
+        case 'png':
+            header('Content-Type: image/png');
+            break;
+        case 'jpg':
+        case 'jpeg':
+            header('Content-Type: image/jpeg');
+            break;
+    }
+    
+    readfile(__DIR__ . '/public' . $uri);
+    return true;
 }
 
 // Route all other requests to index.html
-include_once 'public/index.html';
+include_once __DIR__ . '/public/index.html';
 ?>
